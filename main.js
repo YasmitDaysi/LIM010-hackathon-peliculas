db1918547f23910a1b825ed3d4da43079eb6cd70
YasmitDaysi <yasdayvasand98@gmail.com>
2019-12-26 17:44:12 -0500

    buscador funcional

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1')
     .then((response) => response.json())
     .then(resultado => mostrarpeliculas(resultado));
@@ -11,17 +10,68 @@ fetch('https://api.themoviedb.org/3/tv/popular?api_key=b8902142fac2162fc1931948f
      .then((response)=>response.json())
     .then(contenido => mostrarSeries(contenido));
 
-fetch('https://api.themoviedb.org/3/tv/popular?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1')  
-    .then((response)=>response.json())
+fetch('https://api.themoviedb.org/3/tv/popular?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1')
+    .then((response)=>response.json())  
     .then(contenido => mostrarTodasSeries(contenido));
-
-    fetch(
-        "https://api.themoviedb.org/3/movie/upcoming?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1"
-      )
+fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1")
         .then(response => response.json())
-        .then(resultado => optenerImagenes(resultado));
-      
-const optenerImagenes = (ultimasPeliculas) => {
+        .then(resultado => optenerImagenesCarrousel(resultado));
+
+   Promise.all([
+        fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1'),
+        fetch('https://api.themoviedb.org/3/tv/popular?api_key=b8902142fac2162fc1931948fcaaa1af&language=en-US&page=1')
+      ]).then(([resultadoPeliculas, resultadoSeries]) => {
+        const peliculas = resultadoPeliculas.json();
+        const series = resultadoSeries.json();
+        return Promise.all([peliculas, series])
+      })
+      .then((responseText) => {
+        busquedaPeliculaSeries(responseText);
+    
+      }).catch((err) => {
+        console.log(err);
+      }); 
+
+    const mostrarFiltro = (arrayObjetc,texto)=>{
+         let valorFiltrado = arrayObjetc.filter((element) => 
+             element.titulo.toLowerCase().indexOf(texto.toLowerCase()) !== -1);
+            let result = '';
+             valorFiltrado.forEach(Element => {
+                result += `<img class='cajas ' src="https://image.tmdb.org/t/p/original${Element.imagen}"/>`;
+              });
+              document.getElementById(
+                "vista1"
+              ).innerHTML = result
+              console.log(result);
+    }
+
+    const busquedaPeliculaSeries = (resultadoPeliculasSeries) => {
+         let nuevowArrayObj = [];
+            resultadoPeliculasSeries.forEach(Element => {  
+            nuevowArrayObj = nuevowArrayObj.concat(Element.results); 
+        })
+        const nuevoArrayString = nuevowArrayObj.map(Element => {
+            /* let x = Element.title ? {titulo: Element.title, imagen: Element.poster_path} :
+             Element.name ? {titulo: Element.name, imagen: Element.poster_path} : 'no se encontro';
+console.log(x);*/
+           if(Element.title){
+            return{
+                   titulo: Element.title,
+                   imagen: Element.poster_path,
+               }
+           } else if(Element.name){
+               return{
+                   titulo: Element.name,
+                   imagen: Element.poster_path,
+               }
+           }
+        });
+        document.getElementById('boton-buscar').addEventListener("click",()=>{
+            const valorTexto = document.getElementById('miBusqueda');
+            mostrarFiltro(nuevoArrayString, valorTexto.value);
+         });
+    }
+    const optenerImagenesCarrousel = (ultimasPeliculas) => {
         const imagenes = ultimasPeliculas.results;
         let cont = 0;
         const idAdelante = document.getElementById("idAdelante");
@@ -30,77 +80,56 @@ const optenerImagenes = (ultimasPeliculas) => {
         let titulo = document.getElementById("titulo-panel2");
         const descripcion = document.getElementById("descripcion-panel");
 
-        img.src = "https://image.tmdb.org/t/p/original"+imagenes[0].backdrop_path;
+        img.src = "https://image.tmdb.org/t/p/original"+imagenes[cont].backdrop_path;
         titulo.innerHTML = `<h2>${imagenes[cont].title}</h2>`
-        descripcion.innerHTML =`<p>${imagenes[cont].overview}</p>`
-        cont = 1;
+         descripcion.innerHTML =`<p>${imagenes[cont].overview}</p>`
         idAdelante.addEventListener("click", () => {
             titulo.innerHTML = "";
             descripcion.innerHTML= "";
-             if (cont < imagenes.length) {
-                img.src = "https://image.tmdb.org/t/p/original" + imagenes[cont ].backdrop_path;
+             if (cont < imagenes.length - 1) {
+                cont++;
+                img.src = "https://image.tmdb.org/t/p/original" + imagenes[cont].backdrop_path;
                 let tituloDiv = document.createTextNode(imagenes[cont].title); 
                 titulo.appendChild(tituloDiv)
                 const descripcionText = document.createTextNode(imagenes[cont].overview);
                 descripcion.appendChild(descripcionText);
-                cont++;
               } else {
-                img.src = "https://image.tmdb.org/t/p/original"+imagenes[0].backdrop_path;
+                cont = 0;
+                img.src = "https://image.tmdb.org/t/p/original" +imagenes[0].backdrop_path;
                 titulo.innerHTML = `<h2>${imagenes[0].title}</h2>`
                 descripcion.innerHTML =`<p>${imagenes[0].overview}</p>`
-                cont = 0;
               }
             });
         idAtras.addEventListener("click", () => {
            titulo.innerHTML = "";
            descripcion.innerHTML = "";
           if (cont > 0) {
+            cont--;
             img.src = "https://image.tmdb.org/t/p/original"+imagenes[cont].backdrop_path;
             let tituloDiv = document.createTextNode(imagenes[cont].title); 
                 titulo.appendChild(tituloDiv);
             let descripcionTextt = document.createTextNode(imagenes[cont].overview);
                 descripcion.appendChild(descripcionTextt);
-             cont--;
           } else {
-            img.src = "https://image.tmdb.org/t/p/original"+imagenes[imagenes.length - 1].backdrop_path;
-            titulo.innerHTML = `<h2>${imagenes[imagenes.length -1].title}</h2>`
-            descripcion.innerHTML =`<p>${imagenes[0].overview}</p>`      
+            img.src = "https://image.tmdb.org/t/p/original" + imagenes[imagenes.length - 1].backdrop_path;
+            titulo.innerHTML = `<h2>${imagenes[imagenes.length - 1].title}</h2>`;
+            descripcion.innerHTML =`<p>${imagenes[imagenes.length - 1].overview}</p>` ;     
             cont = imagenes.length - 1;
           }
         });
       };
 
-const contenedorP = document.getElementById("mostrarP");
 
+const contenedorP = document.getElementById("mostrarP");
 const mostrarpeliculas = (peliculas)=>{
-     //console.log(peliculas)
     for(let i = 0; i < 12; i ++){
          const divmostrarP = document.createElement("div");
-        //console.log(divmostrarP);
         divmostrarP.style.backgroundImage = "url('https://image.tmdb.org/t/p/original" + peliculas.results[i].poster_path+"')";
-       //  console.log(peliculas.results[i])
         divmostrarP.setAttribute("class","cajas");
         contenedorP.appendChild(divmostrarP);
     }
 };
-const todasPeliculas = document.getElementById("todasPeliculas");
 
-const mostrarTodaspeliculas =(traeTodasPeliculas)=>{      
-    traeTodasPeliculas.results.forEach((element) => {
-        //console.log(element)
-          const divTodasPeliculas = document.createElement("div");
-          //console.log(divTodasPeliculas);
-          divTodasPeliculas.style.backgroundImage = "url('https://image.tmdb.org/t/p/original" + element.poster_path +"')";
-          //console.log(element);
-          divTodasPeliculas.setAttribute("class","cajas")
-          todasPeliculas.appendChild(divTodasPeliculas);
-          
-
-          //console.log(divmostrarP)
-      });
-      };
-
-//console.log(todasPeliculas);
 const  contenedorS = document.getElementById("mostrarS");
 
 const mostrarSeries = (series)=>{
@@ -112,6 +141,18 @@ const mostrarSeries = (series)=>{
     }
 }
 
+const todasPeliculas = document.getElementById("todasPeliculas");
+
+const mostrarTodaspeliculas =(traeTodasPeliculas)=>{      
+    traeTodasPeliculas.results.forEach((element) => {
+          const divTodasPeliculas = document.createElement("div");
+          divTodasPeliculas.style.backgroundImage = "url('https://image.tmdb.org/t/p/original" + element.poster_path +"')";
+          divTodasPeliculas.setAttribute("class","cajas")
+          todasPeliculas.appendChild(divTodasPeliculas);
+      });
+      };
+
+
 const  contTodasSeries = document.getElementById("todasSeries");
 
 const  mostrarTodasSeries = (todasSeries)=>{
@@ -121,6 +162,9 @@ const  mostrarTodasSeries = (todasSeries)=>{
         divTodasSeries.setAttribute("class","cajas")
         contTodasSeries.appendChild(divTodasSeries);
     });
+}
+const infoSeriePelicula = ()=>{
+
 }
 const vista1 = document.getElementById("vista1");
 const verMasPeliculas = document.getElementById("verMasPeliculas");
@@ -140,4 +184,4 @@ verMasSeries.addEventListener("click",()=>{
 })
 
 
-
+    
